bb4fd1fd128d34214728e642288ae2d879bcae5e
MathieuBK <45979854+MathieuBK@users.noreply.github.com>
2024-04-29 02:07:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -116,8 +116,11 @@ user_prompt = st.text_input(" ",
 # !!! ERROR : "2024-04-22 01:23:31.938 `label` got an empty value. This is discouraged for accessibility reasons and may be disallowed in the future by raising an exception. Please provide a non-empty label and hide it with label_visibility if needed." 
 # ==> #a -> add space in between brackets (i.e. " " instead of "") and label_visibility="collapsed" in st.text_input and all relevant fields
 
-col1, col2 = st.columns([0.52, 0.5])
-col2.caption(":gray[©️ 2024 Copyright [Mathieu Bekkaye](https://mathieubk-personalwebsite.streamlit.app) - All rights reserved.]")
+# col1, col2 = st.columns([0.52, 0.5])
+# col2.caption(":gray[©️ 2024 Copyright [Mathieu Bekkaye](https://mathieubk-personalwebsite.streamlit.app) - All rights reserved.]")
+
+st.markdown("<div style='text-align: right; color: #83858C; font-size:14px;'>&copy; 2024 Copyright <a href='https://www.linkedin.com/in/mathieubekkaye'>Mathieu Bekkaye</a> - All rights reserved.</div>", unsafe_allow_html=True)
+# st.markdown("<div style='text-align: right; color: #83858C; font-size:14px;'>&copy; 2024 Copyright <a href='https://mathieubk-personalwebsite.streamlit.app'>Mathieu Bekkaye</a> - All rights reserved.</div>", unsafe_allow_html=True)
 
 with open('./styles/style.css') as f:
     css = f.read()
